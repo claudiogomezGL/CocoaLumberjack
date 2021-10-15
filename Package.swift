9a8cbcec2c6f6b77197543d0e7d70a6aaeeda97e
Claudio Gomez <claudio.gomez@gorillalogic.com>
2021-10-15 16:04:28 -0600

    Change library name to prevent clash when building

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "CocoaLumberjack",
+            name: "CocoaLumberjackObjC",
             type: .dynamic,
             targets: ["CocoaLumberjack"]),
         .library(
